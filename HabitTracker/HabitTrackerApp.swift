ae41efd54047a56a9598a914afd94e941be6f09b
Burak Cüce <b.cuece@web.de>
2022-08-15 00:13:31 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Burak Cüce on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
